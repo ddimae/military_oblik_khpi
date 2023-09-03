74772d4731113608d22dda641d5a34ae473268bb
Dmytro Dvukhhlavov <ddimae72@gmail.com>
2023-09-03 16:47:20 +0300

    Update MilitaryPersonServiceImpl.java

--- src/main/java/ntukhpi/semit/militaryoblik/service/MilitaryPersonServiceImpl.java
+++ src/main/java/ntukhpi/semit/militaryoblik/service/MilitaryPersonServiceImpl.java
@@ -28,8 +28,9 @@ public class MilitaryPersonServiceImpl implements MilitaryPersonService {
     }
 
     @Override
-    public List<MilitaryPerson> getAllMilitaryPerson() {
-        return militaryPersonRepository.findAll();
+    public MilitaryPerson getMilitaryInfo() {
+        List<MilitaryPerson> list = militaryPersonRepository.findAll();
+        return !list.isEmpty()?list.get(0):null;
     }
 
     @Override
